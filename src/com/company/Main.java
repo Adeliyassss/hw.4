716506381cacd92f0af651878cbbb59436e29d66
Adeliyassss <100111844+Adeliyassss@users.noreply.github.com>
2023-01-17 15:45:32 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Comparator;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter five names: ");
+        ArrayList<String> listA = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String names = sc.next();
+            listA.add(names);
+        }
+        System.out.println("List A: " + listA);
+
+        System.out.println("--------------------------------");
+        System.out.println("Enter more five names: ");
+        ArrayList<String> listB = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String names = sc.next();
+            listB.add(names);
+        }
+        Collections.reverse(listB);
+        System.out.println("Reversed List B: " + listB);
+        ArrayList<String> listC = new ArrayList<>();
+
+        for (int i = 0; i < listB.size(); i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(i));
+        }
+        System.out.println("List C: " + listC);
+        listC.sort(Comparator.comparingInt(String::length));
+        System.out.println(listC);
+    }
+
+}
+
